aed9f1c1fb9895eee29059ca9cb5d68092cc8b8a
heyiamdavid522 <heyiamdavid19@gmail.com>
2026-01-06 00:05:36 -0500

    commit initial

---
+++ player.gd
@@ -0,0 +1,66 @@
+extends CharacterBody3D
+
+var SPEED
+const WALK_SPEED = 7.0
+const SPRINT_SPEED = 9.0
+const JUMP_VELOCITY = 4.5
+const SENSITIVITY = 0.005
+
+const BOB_FREQ = 2.0
+const BOB_AMP = 0.08
+var t_bob = 0.0
+
+var gravity = 9.8
+
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
+
+func _ready():
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	#Handle sprint.
+	if Input.is_action_pressed("sprint"):
+		SPEED = SPRINT_SPEED
+	else:
+		SPEED = WALK_SPEED
+
+	# Controles, velocidad y esas cosas de movimiento
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			velocity.x = lerp(velocity.x, direction.x * SPEED, delta * 2.0)
+			velocity.z = lerp(velocity.z, direction.z * SPEED, delta * 10.5)
+	else:
+		velocity.x = lerp(velocity.x, direction.x * SPEED, delta * 2.0)
+		velocity.z = lerp(velocity.z, direction.z * SPEED, delta * 10.5)
+	
+	#Head bob (El movimiento de la cabeza al caminar/correr)
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = _headbob(t_bob)
+	
+	move_and_slide()
+
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BOB_FREQ) * BOB_AMP
+	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
+	return pos
